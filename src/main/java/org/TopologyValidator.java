cfcebdfd2fc17c556167b0e781979d3290adb67f
Ram <ramanarayan94@gmail.com>
2019-04-22 23:55:43 -0500

    Adding config files

--- src/main/java/org/TopologyValidator.java
+++ src/main/java/org/TopologyValidator.java
@@ -1,22 +1,26 @@
 package org;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Properties;
 
 import org.apache.commons.codec.binary.Base64;
 
 public class TopologyValidator {
 
-	public static String getTopology() {
+	public String getTopologyFromController() {
 		try {
-			String user = "admin";
-		    String password = "admin";
-		    String baseURL = "http://localhost:8181/restconf/operational/network-topology:network-topology/topology/flow:1/";
-		    String containerName = "default";
+			Properties serverProps = new Properties();
+			serverProps.load(new FileInputStream("server_config.properties"));
+			System.out.println(serverProps.getProperty("url"));
+			String user = serverProps.getProperty("user");
+		    String password = serverProps.getProperty("password");
+		    String baseURL = serverProps.getProperty("url");
 			URL url = new URL(baseURL);
 			String authStr = user + ":" + password;
 	        String encodedAuthStr = Base64.encodeBase64String(authStr.getBytes());
@@ -43,10 +47,15 @@ public class TopologyValidator {
 		return null;
 	}
 
+	public static String getTopologyFromFile() {
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World");
-		System.out.println("getTopology " + getTopology());
+		TopologyValidator topo = new TopologyValidator();
+		System.out.println("getTopology " + topo.getTopologyFromController());
 
 	}
 
