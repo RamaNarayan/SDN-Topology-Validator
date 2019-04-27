f4a0c28b0c0087043f9a536450a18782b6948c40
Ram <ramanarayan94@gmail.com>
2019-04-27 12:44:20 -0500

    adding json files and root

---
+++ src/main/java/org/TopologyRoot.java
@@ -0,0 +1,17 @@
+package org;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+public class TopologyRoot {
+	@JsonProperty("topology")
+	private List<Topology> topology;
+}
